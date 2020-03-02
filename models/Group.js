9cd9611553ebac4dce9c4dc1c4f043be8153b5fb
Alexiss Diaz <alexiss.diaz@live.com>
2020-03-02 10:34:21 -0600

    added new colum to table

--- models/Group.js
+++ models/Group.js
@@ -5,6 +5,9 @@ module.exports = function (sequelize, DataTypes) {
     var Group = sequelize.define("Group", {
         name: {
             type: DataTypes.STRING,
+        },
+        wayPoint: {
+            type: DataTypes.STRING,
         }
     });
 
